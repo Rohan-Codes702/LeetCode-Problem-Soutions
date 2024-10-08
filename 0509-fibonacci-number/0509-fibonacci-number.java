1c2aab96cdaa7024a02c817e8c3dd037592ab700
Rohan-Codes702 <rohanmane702@gmail.com>
2024-10-08 22:20:26 +0530

    Time: 10 ms (11.01%), Space: 40.4 MB (38.64%) - LeetHub

---
+++ 0509-fibonacci-number/0509-fibonacci-number.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int fib(int n) {
+        return fibo(n);
+        
+    }
+    public static int fibo(int n)
+    {
+        if(n==1)
+        {
+            return 1;
+        }
+        if(n==0)
+        {
+            return 0;
+        }
+        return fibo(n-1)+fibo(n-2);
+    }
+}
