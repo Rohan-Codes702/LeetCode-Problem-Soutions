0c967777066203d8bdd1b69c9335d2d6db9cc67f
Rohan <rohanmane702@gmail.com>
2025-09-22 19:41:27 +0530

    Time: 2 ms (75.64%), Space: 42.7 MB (16.17%) - LeetHub

--- 3242-count-elements-with-maximum-frequency/3242-count-elements-with-maximum-frequency.java
+++ 3242-count-elements-with-maximum-frequency/3242-count-elements-with-maximum-frequency.java
@@ -1,30 +1,21 @@
 class Solution {
     public int maxFrequencyElements(int[] nums) {
-        int n=nums.length;
+        int n = nums.length;
 
-        HashMap<Integer,Integer>map=new HashMap<>();
+        HashMap<Integer, Integer> map = new HashMap<>();
 
-        for(int i=0;i<n;i++){
-            if(map.containsKey(nums[i])){
-                map.put(nums[i],map.get(nums[i])+1);
-            }
-            else{
-                map.put(nums[i],1);
-            }
+        for (int i = 0; i < n; i++) {
+            map.put(nums[i], map.getOrDefault(nums[i], 0) + 1);
+        }
+        int maxfreq = 0;
+        for (int i : map.keySet()) {
+            maxfreq = Math.max(map.get(i), maxfreq);
         }
 
-        int maxfreq=0;
-
-        for(int key:map.keySet()){
-            maxfreq=Math.max(maxfreq,map.get(key)); 
-        }
-
-
-        int count=0;
-
-        for(int key:map.keySet()){
-            if(map.get(key)==maxfreq){
-                count+=maxfreq;
+        int count = 0;
+        for (int i : map.keySet()) {
+            if (map.get(i) == maxfreq) {
+                count += maxfreq;
             }
         }
         return count;
