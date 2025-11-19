594443041956ccc67c3985ae47651630cfde9184
Rohan <rohanmane702@gmail.com>
2025-11-19 18:07:38 +0530

    Time: 7 ms (7.96%), Space: 46.8 MB (7.36%) - LeetHub

--- 2274-keep-multiplying-found-values-by-two/2274-keep-multiplying-found-values-by-two.java
+++ 2274-keep-multiplying-found-values-by-two/2274-keep-multiplying-found-values-by-two.java
@@ -3,12 +3,7 @@ class Solution {
         int n=nums.length;
         int val=original;
         Arrays.sort(nums);
-        HashSet<Integer>set=new HashSet<>();
-
-        for(int i:nums){
-            set.add(i);
-        }
-
+        
         for(int i=0;i<n;i++){
            if(nums[i]==val){
              val=val*2;
